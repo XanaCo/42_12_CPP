1283651070f99938bceb3007f57cb3278d58e9d6
Ana COLMENERO <ancolmen@bess-f2r3s14.clusters.42paris.fr>
2024-01-29 17:40:16 +0100

    Little Changes

--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -9,9 +9,6 @@
 #define RED "\033[31;1m"
 #define GREEN "\033[32;1m"
 #define YELLOW "\033[33;1m"
-#define BLUE "\033[34;1m"
-#define MAGENTA "\033[35;1m"
-#define CYAN "\033[36;1m"
 #define WHITE "\033[37;1m"
 #define END_COLOR "\033[0m"
 
