c8aa5dfd4233727960c770b49cf1701c02138d87
Ana COLMENERO <ancolmen@bess-f2r1s4.clusters.42paris.fr>
2024-02-01 21:43:50 +0100

    CPP09 started

---
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancolmen <ancolmen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 14:56:29 by ancolmen          #+#    #+#             */
+/*   Updated: 2024/02/01 21:31:59 by ancolmen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <iterator>
+# include <cstdlib>
+# include <set>
+# include <list>
+# include <deque>
+# include <vector>
+# include <exception>
+# include <algorithm>
+
+# define RED "\033[31;1m"
+# define GREEN "\033[32;1m"
+# define YELLOW "\033[33;1m"
+# define WHITE "\033[37;1m"
+# define END_COLOR "\033[0m"
+
+# define PRINT true
+
+///
+
+#endif
---
+++ CPP_09/ex00/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancolmen <ancolmen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 14:56:29 by ancolmen          #+#    #+#             */
+/*   Updated: 2024/02/01 20:54:58 by ancolmen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		// program starts here
+
+	std::cerr << "Error : Wrong number of arguments" << std::endl;
+	return (EXIT_FAILURE);
+}
